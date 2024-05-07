a6870940b4c72168aad5cb8f677c913dfd80ecca
Jeon Jeong min <77835002+imjm@users.noreply.github.com>
2024-05-08 02:04:14 +0900

    [Gold III] Title: 소문난 칠공주, Time: 380 ms, Memory: 218832 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,83 +1,85 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	static final int N = 5;
-	static char[][] a;
-	static int dy[] = {-1, 0, 1, 0};
-	static int dx[] = {0, 1, 0, -1};
-	static boolean[] visited;
-	static int[] numbers;
-	static int ret;
-	// 조합으로 25개 중 연속된 7개 구하기
-	// 연속된 7개가 7공주 가능한지 체크하기
-	// 7공주: 다솜파
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		a = new char[N][N];
-		numbers = new int[7];
-		for(int i=0; i<N; i++) {
-			char[] s = br.readLine().toCharArray();
-			for(int j=0; j<N; j++) {
-				a[i][j] = s[j];
-			}
-		}
-		go(0, 0);
-		System.out.println(ret);
-	} // main
-	static boolean bfs() {
-		visited[0] = true;
-		Queue<Pair> q = new ArrayDeque<Pair>();
-		q.add(new Pair(numbers[0]/5, numbers[0]%5));
-		int cnt = 1;
-		int cntS = 0;
-		if(a[q.peek().y][q.peek().x] == 'S') cntS++;
-		while(!q.isEmpty()) {
-			Pair p = q.poll();
-			for(int i=0; i<4; i++) {
-				int ny = p.y + dy[i];
-				int nx = p.x + dx[i];
-				int ni = ny*5 + nx;
-				if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
-				for(int j=0; j<7; j++) {
-					if(numbers[j] == ni && !visited[j]) {
-						visited[j] = true;
-						if(a[ny][nx] == 'S') cntS++;
-						q.add(new Pair(ny, nx));
-						cnt++;
-					}
-				}
-			}
-		}
-		if(cnt==7 && cntS >= 4) return true;
-		return false;
-	}
-	static void go(int cnt, int start) {
-		if(cnt==7) {
-			visited = new boolean[7];
-			if(bfs()) {
-				ret++;
-			}
-			return;
-		}
-		for(int i=start; i<N*N; i++) {
-			numbers[cnt] = i;
-			go(cnt+1, i+1);
-		}
-	}
-	static class Pair {
-		int y, x;
+    private static final char[][] map = new char[5][5];
+    private static final boolean[][] visited = new boolean[5][5];
+    private static final int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static int cs = 0; //case 경우의 수
 
-		public Pair(int y, int x) {
-			super();
-			this.y = y;
-			this.x = x;
-		}
-		
-	}
-} // class
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        dfs(0, 0); //7자리를 선택한다.
+        System.out.println(cs);
+    }
+
+    private static void dfs(int nowDepth, int start) {
+        if (nowDepth == 7) {
+            if (checkLinked()) cs++;
+        } else {
+            //25개의 자리중 7개를 선택한다 
+            for (int i = start; i < 25; i++) {
+                visited[i / 5][i % 5] = true;
+                dfs(nowDepth + 1, i + 1);
+                visited[i / 5][i % 5] = false;
+            }
+        }
+    }
+
+    private static boolean checkLinked() {
+        //연속된 7자리에 대해서 copy
+        boolean[][] cpyVisited = new boolean[5][5];
+        for (int i = 0; i < 5; i++) {
+            cpyVisited[i] = visited[i].clone();
+        }
+
+        int x = 0, y = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (cpyVisited[i][j]) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        
+        //연속된 7자리인지, 이다솜파 과반수 이상인지 확인
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y));
+        
+        int cnt = 0;
+        int s = 0;
+        while (!q.isEmpty()) {
+            Point poll = q.poll();
+
+            for (int[] dir : dirs) {
+                int mvx = poll.x + dir[0];
+                int mvy = poll.y + dir[1];
+
+                if (0 > mvx || 5 <= mvx || 0 > mvy || 5 <= mvy) { //범위를 넘어감
+                    continue;
+                }
+                
+                if (cpyVisited[mvx][mvy]) {
+                    if (map[mvx][mvy] == 'S') s++; //이다솜파
+                    cnt++; //연결된사람들 확인
+                    q.add(new Point(mvx, mvy));
+                    cpyVisited[mvx][mvy] = false; //방문확인
+                }
+            }
+        }
+        if (cnt == 7 && 4 <= s) { //7개가 연속으로 이어져 있는지, 이다솜파가 4명(과반수 이상인지)
+            return true;
+        }
+        return false;
+    }
+}
