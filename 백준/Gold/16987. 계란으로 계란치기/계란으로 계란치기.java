59c0902e14e0f5497ad3cdbf433ee4aba059d8c6
Jeon Jeong min <77835002+imjm@users.noreply.github.com>
2024-05-22 15:39:26 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 212 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,53 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, ans;
+
+	// 가장 왼쪽 계란부터 들고, 다른 계란을 친다. 손에 든 계란이 깨지거나 칠 계란이 없으면 넘어간다.
+	// 이후 원래 자리에 놓고 다음 계란을 들고 반복한다.
+	// 계란을 드는 과정은 순서대로지만 계란을 깨는 과정을 순서가 없다. -> 순열
+	// 상대 계란의 무게 만큼 내구도가 깎임
+	// 2차원배열로 계란 정보[내구도, 무게]
+	// 배열을 인자로 넘기면서 각 경우마다 계란 정보 유지
+	// visited처리
+	// 종료 조건: idx==n일때
+	static int n, ret=-1;
 	static int[][] egg;
-	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		N = Integer.parseInt(br.readLine());
-		egg = new int[N][2]; // 내구도, 무게 
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(br.readLine());
+		egg = new int[n][2];
+		boolean[] visited = new boolean[n];
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			egg[i][0] = Integer.parseInt(st.nextToken());
 			egg[i][1] = Integer.parseInt(st.nextToken());
 		}
-		
-		ans = 0;
-		solution(0, 0);
-		System.out.println(ans);
-	}
-
-	private static void solution(int now, int breakEgg) {
-		
-		ans = Math.max(ans, breakEgg);
-		if(now == N) return; // 가장 최근에 든 계란이 가장 오른쪽에 위치한 계란
-		if(egg[now][0] <= 0) solution(now+1, breakEgg); // 손에 든 계란이 깨진 경우
-		else {			
-			for(int next=0; next<N; next++) {
-				if(next == now || egg[next][0] <= 0) continue; // 깨지지 않은 다른 계란을 치기 위해
-				
-				// 계란으로 계란을 치게 되면 
-				// 각 계란의 내구도는 상대 계란의 무게만큼 깎임
-				egg[now][0]-=egg[next][1];
-				egg[next][0]-=egg[now][1];
-				
-				int newBreakEgg = breakEgg;
-				if(egg[now][0]<=0) newBreakEgg++;
-				if(egg[next][0]<=0) newBreakEgg++;
-				
-				solution(now+1, newBreakEgg);
-				
-				// 복구
-				egg[now][0]+=egg[next][1];
-				egg[next][0]+=egg[now][1];
+		go(0, 0);
+		System.out.println(ret);
+	} // main
+	
+	static void go(int now, int cnt) {
+		ret = Math.max(cnt, ret);
+		if(now == n) {
+			return;
+		}
+		if(egg[now][0] <= 0) go(now+1, cnt);
+		else {
+			for(int i=0; i<n; i++) {
+				if(i==now || egg[i][0] <= 0) continue;
+				egg[i][0] -= egg[now][1];
+				egg[now][0] -= egg[i][1];
+				int broken = cnt;
+				if(egg[i][0]<=0) broken++;
+				if(egg[now][0]<=0) broken++;
+				go(now+1, broken);
+				egg[i][0] += egg[now][1];
+				egg[now][0] += egg[i][1];
 			}
 		}
 	}
-}
+} // class
