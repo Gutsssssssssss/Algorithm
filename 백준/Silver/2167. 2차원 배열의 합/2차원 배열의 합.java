17fb384ff62c4a8dc1696e6c6fcfaa9de294b932
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2024-12-16 14:45:12 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 408 ms, Memory: 28204 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,38 +1,47 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n][m];
-        // 배열 초기화
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int k = Integer.parseInt(br.readLine());
-        for (int t = 0; t < k; t++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken()) - 1;
-            int j = Integer.parseInt(st.nextToken()) - 1;
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            int y = Integer.parseInt(st.nextToken()) - 1;
-            int sum = 0;
-            
-            // (i,j)부터 (x,y)까지 더하기
-            for (int p = i; p <= x; p++) {
-                for (int q = j; q <= y; q++) {
-                    sum += arr[p][q];
-                }
-            }
-            sb.append(sum + "\n");
-        }
-        System.out.print(sb);
-    }
+	public static int N, M, K;
+	public static int[][] arr;
+	public static int[][] prefixSum;
+	public static long answer;
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    		
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	arr = new int[N][M];
+    	prefixSum = new int[N+1][M+1];
+    	for(int i=0;i<N;i++) {
+    		st = new StringTokenizer(br.readLine());
+    		for(int j=0;j<M;j++) {
+    			arr[i][j] = Integer.parseInt(st.nextToken());
+    		}
+    	}
+    	
+    	for(int i=1;i<=N;i++) {
+    		for(int j=1;j<=M;j++) {
+    			prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + arr[i-1][j-1]; 
+    		}
+    	}
+    	
+    	st = new StringTokenizer(br.readLine());
+    	K = Integer.parseInt(st.nextToken());
+    	for(int t=0;t<K;t++) {
+    		st = new StringTokenizer(br.readLine());
+    		int i = Integer.parseInt(st.nextToken());
+    		int j = Integer.parseInt(st.nextToken());
+    		int x = Integer.parseInt(st.nextToken());
+    		int y = Integer.parseInt(st.nextToken());
+    		
+    		System.out.println(prefixSum[x][y] - prefixSum[i-1][y] - prefixSum[x][j-1] + prefixSum[i-1][j-1]);
+    		
+    	}
+    	
+    	
+	}
 }
