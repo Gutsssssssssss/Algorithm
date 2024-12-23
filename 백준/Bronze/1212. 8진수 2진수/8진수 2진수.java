82097cf3edacc63ca072ab5ae308d9b6ca9e1ce4
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2024-12-23 18:52:18 +0900

    [Bronze II] Title: 8진수 2진수, Time: 288 ms, Memory: 22068 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -4,28 +4,34 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] N = br.readLine().toCharArray();
-		
-		if (N[0] == '0') {
-			System.out.println(0);
-			return;
-		}
-		
-		int first = N[0] - '0';
-		StringBuilder sb = new StringBuilder(Integer.toBinaryString(first));
-		
-		for (int i = 1; i < N.length; i++) {
-			int cur = N[i] - '0';
-			
-			String binary = Integer.toBinaryString(cur);
-			
-			while (binary.length() < 3) {
-				binary = "0" + binary;
-			}
-			sb.append(binary);
-		}
-		System.out.println(sb);
-	} // main
-} // class
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String oct = br.readLine();
+
+        // 입력이 0일 때 바로 0 출력
+        if (oct.equals("0")) {
+            System.out.println("0");
+            return;
+        }
+
+        // 각 8진수 숫자에 대응되는 2진수 문자열 배열
+        String[] octToBin = {
+            "000", "001", "010", "011", "100", "101", "110", "111"
+        };
+
+        StringBuilder sb = new StringBuilder();
+
+        // 첫 번째 8진수 숫자 처리 (앞의 0 제거)
+        int firstDigit = oct.charAt(0) - '0';
+        sb.append(Integer.toBinaryString(firstDigit)); // 직접 처리 (1, 10, 11 등으로 시작)
+
+        // 나머지 숫자들은 항상 3비트로 변환
+        for (int i = 1; i < oct.length(); i++) {
+            int digit = oct.charAt(i) - '0';
+            sb.append(octToBin[digit]);
+        }
+
+        // 최종 출력
+        System.out.println(sb.toString());
+    }
+}
