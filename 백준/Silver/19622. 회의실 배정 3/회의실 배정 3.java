c63692a6b62b7b65fcc0a847fdde68e4cb7cbe6d
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-01-09 18:02:42 +0900

    [Silver II] Title: 회의실 배정 3, Time: 340 ms, Memory: 43964 KB -BaekjoonHub

--- 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.java
+++ 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[][] dp = new int[N+1][2];
+		int[] dp = new int[N+1];
 		int[] arr = new int[N+1];
 		int sum = 0;
 		for (int i = 1; i <= N; i++) {
@@ -20,13 +20,11 @@ public class Main {
 			arr[i] = cnt;
 		}
 		
-		dp[1][1] = arr[1];
-		dp[1][0] = 0;
+		dp[1] = arr[1];
 		
 		for (int i = 2; i <= N; i++) {
-			dp[i][0] = Math.max(dp[i-1][1], dp[i-2][1]);
-			dp[i][1] = Math.max(dp[i-2][1], dp[i-1][0]) + arr[i];
+			dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
 		}
-		System.out.println(Math.max(dp[N][0], dp[N][1]));
+		System.out.println(dp[N]);
 	} // main
 }
