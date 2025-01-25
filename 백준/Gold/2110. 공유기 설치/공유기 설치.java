665e4221970202062ac70b5abe3347b40ca73e46
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-01-25 15:25:07 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 29132 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,52 +5,47 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    
 	static int N, C;
 	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		int mn = Integer.MAX_VALUE / 2;
-		int mx = -1;
-		arr = new int[N];
-		for (int i = 0; i < N; i++) {
+		arr = new int[N+1];
+		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			mx = Math.max(mx, arr[i]);
-			mn = Math.min(mn, arr[i]);
 		}
-		
+
 		Arrays.sort(arr);
-		
-		int lo = 1; int hi = arr[N-1] - arr[0];
-		int ans = 0;
+		int lo = 1; int hi = arr[N] - arr[1];
+		int ret = 0;
+
 		while (lo <= hi) {
 			int mid = (lo + hi) >>> 1;
 			
 			if (check(mid)) {
 				lo = mid + 1;
-				ans = mid;
+				ret = mid;
 			} else {
 				hi = mid - 1;
 			}
 		}
-		System.out.println(ans);
+		System.out.println(ret);
 	} // main
+	
 	static boolean check(int mid) {
-		int ret = 1;
-		int temp = arr[0];
-		for (int i = 1; i < N; i++) {
-			if (arr[i] - temp >= mid) {
-				ret++;
+		int cnt = 1;
+		int temp = arr[1];
+		for (int i = 2; i <= N; i++) {
+			if (temp + mid <= arr[i]) {
+				cnt++;
 				temp = arr[i];
-				if (ret == C) {
-					return true;
-				}
+		
 			}
 		}
-		return (false);
+		return cnt >= C;
 	}
 }
