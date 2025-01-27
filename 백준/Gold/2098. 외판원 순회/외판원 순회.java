7059c45f61ffd70dc23d0f31a46430daa5fc19a5
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-01-27 18:52:25 +0900

    [Gold I] Title: 외판원 순회, Time: 180 ms, Memory: 21436 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,49 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.net.Inet4Address;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int[][] W;
+	static final int INF = 16_000_001;
 	static int N;
-	static int[][] dp;
-	static final int INF = Integer.MAX_VALUE / 2;
+	static int[][] W, dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
 		W = new int[N][N];
-		dp = new int[1 << N][N];
-		
+		dp = new int[(1<<N)][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				W[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		for (int i = 0; i < (1 << N); i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		
 		System.out.println(tsp(1, 0));
 	} // main
 	
-	static int tsp(int visited, int now) {
+	static int tsp(int visited, int here) {
 		if (visited == (1 << N) - 1) {
-			return W[now][0] == 0 ? INF : W[now][0];
+			return W[here][0] == 0 ? INF : W[here][0];
+ 		}
+		
+		if (dp[visited][here] != 0) {
+			return dp[visited][here];
 		}
 		
-		if (dp[visited][now] != -1) return dp[visited][now];
-		dp[visited][now] = INF;
+		dp[visited][here] = INF;
 		
 		for (int i = 0; i < N; i++) {
 			if ((visited & (1 << i)) != 0) continue;
-			if (W[now][i] == 0) continue;
-			dp[visited][now] = Math.min(dp[visited][now], tsp(visited | (1 << i), i) + W[now][i]);
+			if (W[here][i] == 0) continue;
+			dp[visited][here] = Math.min(dp[visited][here], tsp(visited | (1 << i), i) + W[here][i]);
 		}
-		return dp[visited][now];
+		return dp[visited][here];
 	}
-}
+} // class
