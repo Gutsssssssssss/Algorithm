af3c9e73fa39ccc0d334fdfe5539de0d36804128
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-01-30 22:04:41 +0900

    [Silver I] Title: 1로 만들기 2, Time: 156 ms, Memory: 19744 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,39 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int[] dp;
-	static final int INF = Integer.MAX_VALUE / 2;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		dp = new int[N+1];
-		Arrays.fill(dp, INF);
-		System.out.println(go(N));
-		go2(N);
-	} // main
-	static void go2(int N) {
-		if (N == 0) return;
-		System.out.print(N + " ");
-		if (N % 3 == 0 && (dp[N] == dp[N/3] + 1)) go2(N / 3);
-		else if (N % 2 == 0 && (dp[N] == dp[N/2] + 1)) go2(N / 2);
-		else if ((N - 1 >= 0) && (dp[N] == dp[N-1] + 1)) go2(N - 1);
-	}
-	
-	static int go(int N) {
-		if (N == 1) {
-			return dp[N] = 0;
+		Arrays.fill(dp, Integer.MAX_VALUE / 2);
+		dp[0] = 0;
+		dp[1] = 0;
+		for (int i = 2; i <= N; i++) {
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i/3] + 1, dp[i]);
+			}
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i/2] + 1, dp[i]);
+			}
+			dp[i] = Math.min(dp[i-1] + 1, dp[i]);
 		}
-		
-		if (dp[N] != INF) return dp[N];
-		
-		if (N % 3 == 0) dp[N] = Math.min(dp[N], go(N / 3) + 1);
-		if (N % 2 == 0) dp[N] = Math.min(dp[N], go(N / 2) + 1);
-		dp[N] = Math.min(dp[N], go(N - 1) + 1);
-		return dp[N];
+		System.out.println(dp[N]);
+		go(N);
+	} // main
+	static void go(int N) {
+		System.out.print(N + " ");
+		if (N == 1) {
+			return;
+		}
+		if ((N % 3 == 0) && (dp[N] == dp[N/3] + 1)) go(N/3);
+		else if ((N % 2 == 0) && (dp[N] == dp[N/2] + 1)) go(N/2);
+		else if ((N - 1 > 0) && (dp[N] == dp[N-1] + 1)) go(N-1);
 	}
-}
+} // class
