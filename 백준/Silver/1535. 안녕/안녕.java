5bddc965fe90d638e0f36db3b98c666a9cecafd4
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-02 16:01:06 +0900

    [Silver II] Title: 안녕, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -7,12 +7,13 @@ public class Main {
 	
 	static int N;
 	static int[] hpArr, joyArr;
+	static int[][][] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		hpArr = new int[N+1];
 		joyArr = new int[N+1];
-		
+		dp = new int[101][N+1][2];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			hpArr[i] = Integer.parseInt(st.nextToken());
@@ -21,6 +22,13 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			joyArr[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i <= 100; i++) {
+			for (int ii = 0; ii <= N; ii++) {
+				for (int iii = 0; iii <= 1; iii++) {
+					dp[i][ii][iii] = -1;
+				}
+			}
 		}
 		System.out.println(Math.max(go(100 - hpArr[1], 1, 1), go(100, 1, 0)));
 	} // main
@@ -31,9 +39,10 @@ public class Main {
 			return flag == 1 ? joyArr[idx] : 0;
 		}
 		if (idx + 1 > N) return -1;
-
+		if (dp[hp][idx][flag] != -1) return dp[hp][idx][flag];
+		
 		int ret = Math.max(go(hp - hpArr[idx+1], idx + 1, 1), go(hp, idx + 1, 0)) + (flag == 1 ? joyArr[idx] : 0);
 		
-		return ret;
+		return dp[hp][idx][flag] = ret;
 	}
 }
