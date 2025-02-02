dac7f0c4909cf535363fe44f45dc809dfa2e8d95
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-02 17:37:59 +0900

    [Gold IV] Title: 수들의 합 4, Time: 336 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -2,35 +2,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, K;
-	static int[] psum;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		psum = new int[N + 1];
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
+		int[] psum = new int[N+1];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			psum[i] = psum[i - 1] + Integer.parseInt(st.nextToken());
+			psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
 		}
-		
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		long cnt = 0;
 		map.put(0, 1);
-		long sum = 0;
 		for (int i = 1; i <= N; i++) {
 			int ans = map.getOrDefault(psum[i] - K, 0);
-			// 현재 값 넣기, 있으면 ++ 없으면 1
+			
 			map.put(psum[i], map.getOrDefault(psum[i], 0) + 1);
-			sum += ans;
+			
+			cnt += ans;
 		}
-		System.out.println(sum);
+		System.out.println(cnt);
 	} // main
-} // class
+}
