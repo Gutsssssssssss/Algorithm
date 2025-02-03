6a885ab142c81444d7e1e14792869999b4b263df
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-03 21:20:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,30 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N+1];
-		int[] dp = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		Arrays.fill(dp, 1);
-		
+		List<Integer> list = new ArrayList<Integer>();
+		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= i; j++) {
-				if (arr[j] < arr[i]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
+			int cur = arr[i];
+			int pos = Collections.binarySearch(list, cur);
+			if (pos >= 0) {
+				continue;
+			} else {
+				pos = (-pos - 1);
+				if (pos == list.size()) {
+					list.add(cur);
+					cnt++;
+				} else {
+					list.set(pos, cur);
 				}
 			}
 		}
-		System.out.println(Arrays.stream(dp).max().getAsInt());
+		System.out.println(cnt);
 	} // main
+	
 }
