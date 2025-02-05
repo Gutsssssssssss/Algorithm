7b2c341489fad58c897c1a29cd446917350ca6a5
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-05 14:16:29 +0900

    [Gold IV] Title: 사탕 가게, Time: 632 ms, Memory: 31876 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -1,33 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
-			String s = st.nextToken();
-			String[] srr = s.split("\\.");
-			
-			
-			int M = Integer.parseInt(srr[0]) * 100 + Integer.parseInt(srr[1]);
-			if (N == 0 && M == 0) break;
-			int[] dp = new int[M+1];
+			String[] sr = st.nextToken().split("\\.");
+			int K = Integer.parseInt(sr[0]) * 100 + Integer.parseInt(sr[1]);
+			if (N == 0 && K == 0) {
+				break;
+			}
+			int[] dp = new int[K+1];
 			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				srr = st.nextToken().split("\\.");
-				int b = Integer.parseInt(srr[0]) * 100 + Integer.parseInt(srr[1]);
-				
-				for (int j = b; j <= M; j++) {
-					dp[j] = Math.max(dp[j], dp[j-b] + a);
+				int c = Integer.parseInt(st.nextToken());
+				String[] sr2 = st.nextToken().split("\\.");
+				int p = Integer.parseInt(sr2[0]) * 100 + Integer.parseInt(sr2[1]);
+				for (int j = p; j <= K; j++) {
+					dp[j] = Math.max(dp[j], dp[j-p] + c);
 				}
+
 			}
-			System.out.println(dp[M]);
+			System.out.println(dp[K]);
 		}
 	} // main
+
 }
