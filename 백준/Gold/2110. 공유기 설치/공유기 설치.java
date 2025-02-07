e923c496b9a9899d4be68407b6181fe2b6134c69
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-07 12:24:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 29128 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,28 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     
-	static int N, C;
+	static int N, M;
 	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1];
-		for (int i = 1; i <= N; i++) {
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-
+		
 		Arrays.sort(arr);
-		int lo = 1; int hi = arr[N] - arr[1];
+		
+		int lo = 1; int hi = arr[N-1] - arr[0];
+		
 		int ret = 0;
-
 		while (lo <= hi) {
 			int mid = (lo + hi) >>> 1;
 			
@@ -35,17 +42,16 @@ public class Main {
 		}
 		System.out.println(ret);
 	} // main
-	
+
 	static boolean check(int mid) {
+		int temp = arr[0];
 		int cnt = 1;
-		int temp = arr[1];
-		for (int i = 2; i <= N; i++) {
+		for (int i = 1; i < N; i++) {
 			if (temp + mid <= arr[i]) {
-				cnt++;
 				temp = arr[i];
-		
+				cnt++;
 			}
 		}
-		return cnt >= C;
+		return cnt >= M;
 	}
 }
