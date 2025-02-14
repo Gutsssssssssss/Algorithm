71c616cf6e4c3cbe9706c49e2bff049cc1806b07
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-14 15:58:12 +0900

    [Gold IV] Title: 수들의 합 4, Time: 320 ms, Memory: 34164 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -1,32 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] psum = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
-		}
-		HashMap<Integer, Integer> map = new HashMap<>();
-		long cnt = 0;
-		map.put(0, 1);
-		for (int i = 1; i <= N; i++) {
-			int ans = map.getOrDefault(psum[i] - K, 0);
-			
-			map.put(psum[i], map.getOrDefault(psum[i], 0) + 1);
-			
-			cnt += ans;
-		}
-		System.out.println(cnt);
-	} // main
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] psum = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+        	psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
+        }
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, 1);
+        
+        long sum = 0;
+        for (int i = 1; i <= N; i++) {
+        	int ans = map.getOrDefault(psum[i] - K, 0);
+        	map.put(psum[i], map.getOrDefault(psum[i], 0) + 1);
+        	sum += ans;
+        }
+        System.out.println(sum);
+    } // main
+    
 }
