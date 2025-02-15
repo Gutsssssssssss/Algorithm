a6876ca6656c22ccab34154d9ea679cee9ee9556
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-15 15:27:47 +0900

    [Gold II] Title: 경로 찾기, Time: 304 ms, Memory: 47380 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.java
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.java
@@ -1,74 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Inet4Address;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static final int MOD = 1_000_007;
+
+	static final int INF = 1_000_007;
 	static int N, M, C;
-	static int[][] map;
+	static int[][] arr;
 	static int[][][][] dp;
-	static int[] dy = {1, 0};
-	static int[] dx = {0, 1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		dp = new int[N][M][C+1][C+1];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				for (int k = 0; k <= C; k++) {
-					Arrays.fill(dp[i][j][k], -1);
-				}
-			}
-		}
-		for (int i = 1; i <= C; i++) {
-			st = new StringTokenizer(br.readLine());
-			map[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = i;
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i <= C; i++) {
-			int ans = 0;
-			if (map[0][0] != 0) {
-				ans = go(0, 0, map[0][0], i - 1);
-			} else {
-				ans = go(0, 0, 0, i);
-			}
-			sb.append(ans).append(" ");
-		}
-		System.out.println(sb);
-	} // main
-	static int go(int y, int x, int prev, int cnt) {
-		if (cnt < 0) return 0;
-		
-		if (y == (N - 1) && x == (M - 1) && cnt == 0) {
-			return 1;
-		}
-		if (dp[y][x][prev][cnt] != -1) return dp[y][x][prev][cnt];
-		int ret = 0;
-		for (int i = 0; i < 2; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-			if (map[ny][nx] != 0) {
-				if (map[ny][nx] > prev) {
-					ret += (go(ny, nx, map[ny][nx], cnt - 1) % MOD);
-				}
-			} else {
-				ret += (go(ny, nx, prev, cnt) % MOD);
-			}
-		}
-		return dp[y][x][prev][cnt] = (ret % MOD);
-	}
-} // class
+	static int[] dy = {0, 1};
+	static int[] dx = {1, 0};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N+1][M+1];
+        dp = new int[N+1][M+1][C+1][C+1];
+        for (int i = 0; i < N + 1; i++) {
+        	for (int ii = 0; ii < M + 1; ii++) {
+        		for (int iii = 0; iii < C + 1; iii++) {
+        			Arrays.fill(dp[i][ii][iii], -1);
+        		}
+        	}
+        }
+        for (int i = 1; i <= C; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = i;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= C; i++) {
+        	if (arr[1][1] != 0) {
+        		sb.append(go(1, 1, arr[1][1], i - 1)).append(" ");
+        	} else {
+        		sb.append(go(1, 1, 0, i)).append(" ");
+        	}
+        }
+        
+        System.out.println(sb);
+    } // main
+    
+    static int go(int y, int x, int prev, int cnt) {
+    	if (cnt < 0) return 0;
+    	
+    	if (y == N && x == M && cnt == 0) {
+    		return 1;
+    	}
+    	if (dp[y][x][prev][cnt] != -1) {
+    		return dp[y][x][prev][cnt];
+    	}
+    	int ret = 0;
+    	for (int i = 0; i < 2; i++) {
+    		int ny = y + dy[i];
+    		int nx = x + dx[i];
+    		if (ny < 1 || ny >= N + 1 || nx < 1 || nx >= M + 1) continue;
+    		if (arr[ny][nx] != 0) {
+    			if (arr[ny][nx] > prev) {
+        			ret += go(ny, nx, arr[ny][nx], cnt - 1) % INF;
+        		}
+    		} else {
+    			ret += go(ny, nx, prev, cnt) % INF;
+    		}
+    	}
+    	
+    	return dp[y][x][prev][cnt] = ret % INF;
+    }
+}
