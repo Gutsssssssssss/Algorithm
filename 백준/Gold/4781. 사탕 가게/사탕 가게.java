dea0e658af7312aebaee5d40a651a85ca9184003
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-15 15:09:53 +0900

    [Gold IV] Title: 사탕 가게, Time: 584 ms, Memory: 31076 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -1,39 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int N = Integer.parseInt(st.nextToken());
+        	String[] s = st.nextToken().split("\\.");
+        	int M = Integer.parseInt(s[0]) * 100 + Integer.parseInt(s[1]);
+        	if (N == 0 && M == 0) break;
+        	
+        	int[] dp = new int[M+1];
+        	for (int i = 1; i <= N; i++) {
+        		st = new StringTokenizer(br.readLine());
+            	int W = Integer.parseInt(st.nextToken());
+            	String[] s2 = st.nextToken().split("\\.");
+            	int v = Integer.parseInt(s2[0]) * 100 + Integer.parseInt(s2[1]);
+            	for (int j = v; j <= M; j++) {
+            		dp[j] = Math.max(dp[j], dp[j-v] + W);
+            	}
+        	}
+        	System.out.println(dp[M]);
+        }
+    } // main
     
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			String[] sr = st.nextToken().split("\\.");
-			int K = Integer.parseInt(sr[0]) * 100 + Integer.parseInt(sr[1]);
-			if (N == 0 && K == 0) {
-				break;
-			}
-			int[] dp = new int[K+1];
-			for (int i = 1; i <= N; i++) {
-				st = new StringTokenizer(br.readLine());
-				int c = Integer.parseInt(st.nextToken());
-				String[] sr2 = st.nextToken().split("\\.");
-				int p = Integer.parseInt(sr2[0]) * 100 + Integer.parseInt(sr2[1]);
-				for (int j = p; j <= K; j++) {
-					dp[j] = Math.max(dp[j], dp[j-p] + c);
-				}
-
-			}
-			System.out.println(dp[K]);
-		}
-	} // main
-
 }
