ad4ea8fafc0090a05e8baef60cd40d14351a373f
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-16 20:53:59 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 888 ms, Memory: 222428 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,57 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    
+
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N+1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int[] idxArr = new int[N+1];
-		List<Integer> list = new ArrayList<Integer>();
-		int cnt = 0;
-		for (int i = 1; i <= N; i++) {
-			int cur = arr[i];
-			int pos = Collections.binarySearch(list, cur);
-			if (pos < 0) {
-				pos = -pos - 1;
-			}
-			
-			if (pos == list.size()) {
-				list.add(cur);
-			} else {
-				list.set(pos, cur);
-			}
-			idxArr[i] = pos;
-		}
-		cnt = list.size();
-		System.out.println(cnt);
-		Stack<Integer> stk = new Stack<Integer>();
-		int idx = cnt;
-		for (int i = N; i >= 1; i--) {
-			if (idxArr[i] == idx - 1) {
-				stk.add(arr[i]);
-				idx--;
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		while (!stk.isEmpty()) {
-			sb.append(stk.pop()).append(" ");
-		}
-		System.out.println(sb);
-	} // main
-	
-	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] idxArr = new int[N+1];
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= N; i++) {
+        	int cur = arr[i];
+        	int pos = Collections.binarySearch(list, cur);
+        	if (pos < 0) {
+        		pos = -pos - 1;
+        	}
+        	
+        	if (pos == list.size()) {
+        		list.add(cur);
+        	} else {
+        		list.set(pos, cur);
+        	}
+        	idxArr[i] = pos;
+        }
+       
+        System.out.println(list.size());
+        int start = list.size() - 1;
+        Stack<Integer> stk = new Stack<Integer>();
+        for (int i = N; i >= 1; i--) {
+        	if (idxArr[i] == start) {
+        		stk.add(arr[i]);
+        		start--;
+        	}
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stk.isEmpty()) {
+        	sb.append(stk.pop()).append(" ");
+        }
+        System.out.println(sb);
+    } // main
 }
