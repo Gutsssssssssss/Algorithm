cb67aab165c659eccb54744df43e876251bff1e9
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-25 16:29:58 +0900

    [Gold V] Title: 1학년, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -1,49 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	static int N;
 	static int[] arr;
 	static long[][] dp;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		arr = new int[N+1];
-		dp = new long[N+1][21];
-		
-		for (int i = 0; i <= N; i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		System.out.println(go(1, arr[1]));
-	} // main
-	
-	static long go(int here, int cur) {
-		if (cur < 0 || cur > 20) {
-			return 0;
-		}
-		
-		if (here == N - 1) {
-			return cur == arr[here + 1] ? 1 : 0;
-		}
-		
-		if (dp[here][cur] != -1) {
-			return dp[here][cur];
-		}
-		
-		long ret = 0;
-		ret += go(here + 1, cur + arr[here + 1]);
-		ret += go(here + 1, cur - arr[here + 1]);
-		
-		return dp[here][cur] = ret;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        arr = new int[N];
+        dp = new long[N][21];
+        for (int i = 0; i < N; i++) {
+        	Arrays.fill(dp[i], -1);
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        long ans = go(1, arr[0]);
+        System.out.println(ans);
+    } // main
+    
+    // 0 이상 20 이하
+    static long go(int idx, int cur) {
+    	if (cur < 0 || cur > 20) {
+    		return 0;
+    	}
+    	if (idx == N - 1) {
+    		if (cur == arr[N-1])
+    			return 1;
+    		else return 0;
+    	}
+    	if (dp[idx][cur] != -1) {
+    		return dp[idx][cur];
+    	}
+    	long ret = 0;
+    	ret += go(idx + 1, cur + arr[idx]);
+    	ret += go(idx + 1, cur - arr[idx]);
+    	
+    	return dp[idx][cur] =  ret;
+    }
 }
