f4db838284d1d6b4632eff6120bf2172159a43c4
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-02-27 16:06:57 +0900

    [Gold II] Title: 컵라면, Time: 688 ms, Memory: 72488 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -1,46 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		List<int[]> list = new ArrayList<int[]>();
-		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			list.add(new int[] {
-					Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken())
-			});
-		}
-		
-		Collections.sort(list, (a, b) -> a[0] - b[0]);
-		
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		for (int[] cur : list) {
-			int cd = cur[0];
-			int cp = cur[1];
-			
-			if (cd > pq.size()) {
-				pq.add(cp);
-			} else if (cp > pq.peek()) {
-				pq.poll();
-				pq.add(cp);
-			}
-		}
-		
-		int sum = 0;
-		while (!pq.isEmpty()) {
-			sum += pq.poll();
-		}
-		System.out.println(sum);
-	} // main
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<int[]> list = new ArrayList<int[]>();
+        
+        for (int i = 0; i < N; i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	list.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+        
+        Collections.sort(list, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int[] cur : list) {
+        	int d = cur[0];
+        	int p = cur[1];
+        	
+        	if (pq.size() < d) {
+        		pq.add(p);
+        	} else if (pq.peek() < p) {
+        		pq.poll();
+        		pq.add(p);
+        	}
+        }
+        int sum = 0;
+        while (!pq.isEmpty()) {
+        	sum += pq.poll();
+        }
+        System.out.println(sum);
+    } // main
+    
 }
