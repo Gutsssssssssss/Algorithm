0528f7c91b9ed5b92c94d6240c877848e020c942
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-04 09:50:50 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 552 ms, Memory: 58888 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,53 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		List<Integer>[] adj = new ArrayList[N+1];
-		for (int i = 1; i <= N; i++) {
-			adj[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < N - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			
-			adj[A].add(B);
-			adj[B].add(A);
-		}
-		
-		boolean[] visited = new boolean[N+1];
-		int[] parent = new int[N+1];
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		
-		q.add(1);
-		visited[1] = true;
-		
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			for (int next : adj[cur]) {
-				if (visited[next]) continue;
-				visited[next] = true;
-				q.add(next);
-				parent[next] = cur;
-			}
-		}
-		
-		for (int i = 2; i <= N; i++) {
-			System.out.println(parent[i]);
-		}
-	} // main
-	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        
+        for (int i = 0; i <= N; i++) {
+        	list.add(new ArrayList<Integer>());
+        }
+        
+        for (int i = 0; i < N - 1; i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int A = Integer.parseInt(st.nextToken());
+        	int B = Integer.parseInt(st.nextToken());
+        	
+        	list.get(A).add(B);
+        	list.get(B).add(A);
+        }
+        
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] parent = new int[N+1];
+        
+        visited[1] = true;
+        q.add(1);
+        
+        while (!q.isEmpty()) {
+        	int here = q.poll();
+        	for (int next : list.get(here)) {
+        		if (visited[next]) continue;
+        		visited[next] = true;
+        		parent[next] = here;
+        		q.add(next);
+        	}
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+        	sb.append(parent[i]).append("\n");
+        }
+        System.out.println(sb);
+    } // main
+    
 }
