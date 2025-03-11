59ebe40649ff8f411626b147a41268feaf6f8a5d
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-11 23:59:07 +0900

    [Silver III] Title: 교수가 된 현우, Time: 276 ms, Memory: 26776 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.java
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.java
@@ -1,25 +1,24 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int TC = 1; TC <= T; TC++) {
-			int N = Integer.parseInt(br.readLine());
-			
-			int sum = 0;
-			for (int i = 5; i <= N; i*=5) {
-				sum += (N / i);
-			}
-			sb.append(sum).append("\n");
-		}
-		System.out.println(sb);
-	} // main
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int T = 1; T <= TC; T++) {
+        	int N = Integer.parseInt(br.readLine());
+        	
+        	int cnt = 0;
+        	for (int i = 5; i <= N; i *= 5) {
+        		cnt += (N / i);
+        	}
+        	sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    } // main
+    
 }
