26963421f5f700b041dc3d4269bfa7a4ee7ff501
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-11 23:37:32 +0900

    [Silver I] Title: 완전 이진 트리, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.java
@@ -2,14 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	
-	static int N;
+	static int K, N;
+	static List<List<Integer>> tree;
 	static int[] arr;
-	static List<List<Integer>> list;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
+        
+        K = Integer.parseInt(br.readLine());
+        tree = new ArrayList<List<Integer>>();
+        for (int i = 0; i < K; i++) {
+        	tree.add(new ArrayList<Integer>());
+        }
         
         N = (int) Math.pow(2, K) - 1;
         arr = new int[N];
@@ -18,31 +22,24 @@ public class Main {
         	arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        list = new ArrayList<List<Integer>>();
-        for (int i = 0; i < N; i++) {
-        	list.add(new ArrayList<Integer>());
-        }
-        
-        // 0 ~ N-1
-        
+        go(0, N - 1, 0);
         StringBuilder sb = new StringBuilder();
-        int s = 0; int e = N - 1;
-        go(s, e, 0);
-        for (List<Integer> i : list) {
-        	for (int j : i) {
+        for (int i = 0; i < K; i++) {
+        	for (int j : tree.get(i)) {
         		sb.append(j).append(" ");
         	}
         	sb.append("\n");
         }
+        
         System.out.println(sb);
     } // main
     
     static void go(int s, int e, int level) {
     	if (s > e) return;
     	int mid = (s + e) >>> 1;
-    	list.get(level).add(arr[mid]);
+    	tree.get(level).add(arr[mid]);
     	go(s, mid - 1, level + 1);
     	go(mid + 1, e, level + 1);
     }
-
+    
 }
