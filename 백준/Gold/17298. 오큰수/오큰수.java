e9247d25589df4eaa0ecbff50c4108828381eed4
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-12 01:35:34 +0900

    [Gold IV] Title: 오큰수, Time: 904 ms, Memory: 148780 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,34 +1,35 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Stack<Integer> stk = new Stack<Integer>();
-		int[] ans = new int[N];
-		Arrays.fill(ans, -1);
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			while (!stk.isEmpty() && arr[stk.peek()] < arr[i]) {
-				ans[stk.peek()] = arr[i];
-				stk.pop();
-			}
-			stk.add(i);
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0 ; i < N; i++)
-		sb.append(ans[i]).append(" ");
-		System.out.println(sb);
-	} // main
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] ans = new int[N];
+        Arrays.fill(ans, -1);
+        Stack<Integer> stk = new Stack<Integer>();
+        for (int i = 0; i < N; i++) {
+        	while (!stk.isEmpty() && arr[stk.peek()] < arr[i]) {
+        		ans[stk.pop()] = arr[i];
+        	}
+        	stk.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+        	sb.append(ans[i]).append(" ");
+        }
+        System.out.println(sb);
+    } // main
+   
 }
