3ab520af75d6d90fe9d3eb525bbd47f3348e5c4a
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-12 01:02:26 +0900

    [Silver I] Title: 용돈 관리, Time: 236 ms, Memory: 23072 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,54 +1,49 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
 	static int N, M;
 	static int[] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		int mx = -1;
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			mx = Math.max(mx, arr[i]);
-			sum += arr[i];
-		}
-		
-		int lo = mx; int hi = sum;
-		int ret = 0;
-		while (lo <= hi) {
-			int mid = (lo + hi) >>> 1;
-			if (check(mid)) {
-				hi = mid - 1;
-				ret = mid;
-			} else {
-				lo = mid + 1;
-			}
-		}
-		System.out.println(ret);
-	} // main
-	
-	static boolean check(int mid) {
-		int temp = mid;
-		int cnt = 1;
-		for (int i = 0; i < N; i++) {
-			if (temp < arr[i]) {
-				temp = mid;
-				cnt++;
-			}
-			temp -= arr[i];
-		}
-		return M >= cnt;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N];
+        int lo = 0;
+        int hi = 0;
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        	lo = Math.max(arr[i], lo);
+        	hi += arr[i];
+        }
+        
+        int ret = 0;
+        while (lo <= hi) {
+        	int mid = (lo + hi) >>> 1;
+        	if (check(mid)) {
+        		hi = mid - 1;
+        		ret = mid;
+        	} else {
+        		lo = mid + 1;
+        	}
+        }
+        System.out.println(ret);
+    } // main
+    
+    static boolean check(int mid) {
+    	int cnt = 1;
+    	int temp = mid;
+    	for (int i = 0; i < N; i++) {
+    		if (temp - arr[i] < 0) {
+    			temp = mid;
+    			cnt++;
+    		}
+    		temp -= arr[i];
+    	}
+    	return cnt <= M;
+    }
 }
-
