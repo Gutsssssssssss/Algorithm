ddabbfdfb1c1e1fba1d3ad4ca0b13c9687f40b92
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-03-18 20:35:32 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 244 ms, Memory: 48776 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,44 +1,37 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] visited = new int[200001];
-		long[] cnt = new long[200001];
-		
-		visited[N] = 1;
-		cnt[N] = 1;
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		q.add(N);
-		
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for (int next : List.of(cur - 1, cur + 1, cur * 2)) {
-				if (next < 0 || next > 200000) continue;
-				if (visited[next] == 0) {
-					visited[next] = visited[cur] + 1;
-					q.add(next);
-					cnt[next] += cnt[cur];
-				} else if (visited[next] == visited[cur] + 1){
-					cnt[next] += cnt[cur];
-				}
-			}
-		}
-		System.out.println(visited[K] - 1);
-		System.out.println(cnt[K]);
-	} // main
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] visited = new int[200001];
+        visited[N] = 1;
+        q.add(N);
+        int[] cnt = new int[200001];
+        cnt[N] = 1;
+        
+        while (!q.isEmpty()) {
+        	int now = q.poll();
+        	for (int next : List.of(now + 1, now - 1, now * 2) ) {
+        		if (next < 0 || next > 200000) continue;
+        		if (visited[next] == 0) {
+        			visited[next] = visited[now] + 1;
+        			q.add(next);
+        			cnt[next] += cnt[now];
+        		} else if (visited[next] == visited[now] + 1) {
+        			cnt[next] += cnt[now];
+        		}
+        	}
+        }
+        System.out.println(visited[M] - 1);
+        System.out.println(cnt[M]);
+    } // main
 }
