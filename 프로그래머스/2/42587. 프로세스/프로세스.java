f51945c7e7c2908e97304ddb416ff1fe6827ac4d
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-03 21:06:57 +0900

    [level 2] Title: 프로세스, Time: 1.36 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,33 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < priorities.length; i++) {
-            q.add(i);
-        }
         int answer = 0;
-        boolean[] check = new boolean[priorities.length];
-        int cnt = 1;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            boolean flag = false;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int p : priorities) {
+            pq.add(p);
+        }
+        
+        while (!pq.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
-                if (check[i]) continue;
-                if (priorities[i] > priorities[cur]) {
-                    flag = true;
-                    break;
+                if (priorities[i] == pq.peek()) {
+                    answer++;
+                    pq.poll();
+                    
+                    if (location == i) {
+                        return answer;
+                    }
                 }
             }
-            if (flag) {
-                q.add(cur);
-            } else {
-                if (cur == location) {
-                    answer = cnt;
-                }
-                check[cur] = true;
-                cnt++;    
-            }
-            
         }
         
         return answer;
