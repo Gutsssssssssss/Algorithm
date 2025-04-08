0f42568f60c14ba786ac1cd77e0b55a83f74379e
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-08 15:32:33 +0900

    [level 2] Title: 소수 찾기, Time: 106.15 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,45 +3,38 @@ import java.util.*;
 class Solution {
     int N;
     String numbers;
-    boolean[] che;
-    int answer;
-    Set<Integer> set = new HashSet<>();
+    Set<Integer> set;
     public int solution(String numbers) {
-        this.numbers = numbers;
-        che = new boolean[100000000];
         N = numbers.length();
-        Arrays.fill(che, true);
-        che[0] = che[1] = false;
-        for (int i = 2; i * i <= 9999999; i++) {
-            if (!che[i]) continue;
-            for (int j = i * i; j <= 9999999; j += i) {
-                che[j] = false;
-            }
-        }
-        // true -> 소수
+        this.numbers = numbers;
+        set = new HashSet<>();
         
-        answer = 0;
         boolean[] visited = new boolean[N];
-        List<Integer> list = new ArrayList<>();
         go(visited, 0, "");
+        
+        int answer = 0;
         for (int i : set) {
-            System.out.println(i);
-            if (che[i]) answer++;
+            if (check(i)) answer++;
         }
         return answer;
+    } // main
+    
+    boolean check(int val) {
+        if (val < 2) return false;
+        for (int i = 2; i < val; i++) {
+            if (val % i == 0) return false;
+        }
+        return true;
     }
     
     void go(boolean[] visited, int depth, String s) {
-        
-        if (depth > N) return;
-        
+        if (depth == N) return;
         for (int i = 0; i < N; i++) {
             if (visited[i]) continue;
             visited[i] = true;
             set.add(Integer.parseInt(s + numbers.charAt(i)));
             go(visited, depth + 1, s + numbers.charAt(i));
             visited[i] = false;
-            
         }
     }
 }
