e39dfdcd5a030f326586edffdac104e24516ac50
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-08 16:38:51 +0900

    [level 2] Title: 피로도, Time: 3.55 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,46 +1,25 @@
-import java.util.*;
-
 class Solution {
     int N;
     int[][] dungeons;
     int ret;
-    int k;
     public int solution(int k, int[][] dungeons) {
-        this.k = k;
+        this.N = dungeons.length;
         this.dungeons = dungeons;
-        N = dungeons.length;
         boolean[] visited = new boolean[N+1];
-        List<Integer> list = new ArrayList<>();
-        go(visited, 0, list);
+        dfs(k, visited, 0);
         int answer = ret;
         return answer;
     }
-    int solve(List<Integer> list) {
-        int temp = k;
-        int ret = 0;
-        for (int i : list) {
-            int needP = dungeons[i][0];
-            int minuP = dungeons[i][1];
-            if (temp >= needP) {
-                temp -= minuP;
-                ret++;
-            } else break;
-        }
-        return ret;
-    }
     
-    void go(boolean[] visited, int depth, List<Integer> list) {
-        if (depth == N) {
-            ret = Math.max(ret, solve(list));
-            return;
-        }
+    void dfs(int hp, boolean[] visited, int depth) {
+        
         for (int i = 0; i < N; i++) {
             if (visited[i]) continue;
+            if (hp < dungeons[i][0]) continue;
             visited[i] = true;
-            list.add(i);
-            go(visited, depth + 1, list);
-            list.remove(list.size() - 1);
+            dfs(hp - dungeons[i][1], visited, depth + 1);
             visited[i] = false;
         }
+        ret = Math.max(ret, depth);
     }
 }
