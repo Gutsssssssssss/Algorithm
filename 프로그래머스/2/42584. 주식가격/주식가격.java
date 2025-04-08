72d7e9751c6f1cf184e8a8180c130b73aecce5ee
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-08 09:39:44 +0900

    [level 2] Title: 주식가격, Time: 32.61 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,27 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         Stack<Integer> stk = new Stack<>();
-        
         int N = prices.length;
         int[] answer = new int[N];
         for (int i = 0; i < N; i++) {
-            int cnt = 1;
-            
             while (!stk.isEmpty() && prices[stk.peek()] > prices[i]) {
-                int t = stk.pop();
-                
-                answer[t] = i-t;       
-                
+                int top = stk.pop();
+                answer[top] = (i - top);
             }
             stk.add(i);
         }
-        System.out.println(stk.size());
         while (!stk.isEmpty()) {
-            int t = stk.pop();
-            answer[t] = N - t - 1;
-        }
-        
+                int top = stk.pop();
+                answer[top] = (N - top - 1);
+            }
         
         return answer;
-    } // main
+    }
 }
