4d7e93b5b88f0945fc8ede955c7cf661bd0ca9d5
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-09 18:06:04 +0900

    [level 2] Title: 모음 사전, Time: 50.94 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,28 +1,24 @@
 import java.util.*;
-
 class Solution {
-    String[] moem = {"A", "E", "I", "O", "U"};
-    Set<String> list = new HashSet<>();
+    int answer = 0;
+    int cnt = 0;
+    String aeiou = "AEIOU";
     public int solution(String word) {
-        go(0, "");
-        List<String> setList = new ArrayList<>(list);
-        Collections.sort(setList);
-        int answer = 0;
-        for (int i = 0; i < setList.size(); i++) {
-            if (setList.get(i).equals(word)) answer = i;
+        dfs("", word);
+        return answer;
+    }
+    
+    void dfs(String cur, String target) {
+        if (cur.length() > 5) return;
+        if (!cur.equals("")) cnt++;
+        if (cur.equals(target)) {
+            answer = cnt;
+            return;
         }
         
-        return answer + 1;
-    }
-    void go(int idx, String cur) {
-        if (idx > 4) return;
         
         for (int i = 0; i < 5; i++) {
-            go(idx + 1, cur + moem[i]);
-            if (!list.contains(cur + moem[i])) list.add(cur + moem[i]);
-            // System.out.println(cur);
-            go(idx + 1, cur);
-            if (!cur.equals("") && !list.contains(cur)) list.add(cur);
+            dfs(cur + "AEIOU".charAt(i), target);
         }
     }
 }
