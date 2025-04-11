957003f74bcfeaa3411f6028e44e9a12d81d75cd
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-04-11 22:13:24 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1520 ms, Memory: 377284 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,72 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = s.charAt(j) - '0';
+			}
+		}
+		
+		boolean[][][] visited = new boolean[N][M][K + 1];
+		Queue<Node> q = new LinkedList<>();
+		visited[0][0][K] = true;
+		q.add(new Node(0, 0, 1, K));
+		
+		int ans = -1;
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+			int cy = cur.y;
+			int cx = cur.x;
+			if (cy == N - 1 && cx == M - 1) {
+				ans = cur.dist;
+				break;
+			}
+			for (int d = 0; d < 4; d++) {
+				int ny = cy + dy[d];
+				int nx = cx + dx[d];
+				if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx][cur.cnt]) continue;
+				if (arr[ny][nx] == 0) {
+					visited[ny][nx][cur.cnt] = true;
+					q.add(new Node(ny, nx, cur.dist + 1, cur.cnt));
+				} else {
+					if (cur.cnt > 0) {
+						visited[ny][nx][cur.cnt] = true;
+						q.add(new Node(ny, nx, cur.dist + 1, cur.cnt - 1));
+					}
+				}
+			}
+		}
+		System.out.println(ans);
+	} // main
+	
+	static class Node {
+		int y;
+		int x;
+		int dist;
+		int cnt;
+		Node(int y, int x, int dist, int cnt) {
+			this.y = y;
+			this.x = x;
+			this.dist = dist;
+			this.cnt = cnt;
+		}
+	}
+}
