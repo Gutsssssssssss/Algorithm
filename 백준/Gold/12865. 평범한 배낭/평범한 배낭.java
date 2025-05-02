69399d2a0848e65a8de27f2f3664652b208bf766
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-05-02 15:29:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,31 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
-public class Main {
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int W = Integer.parseInt(st.nextToken());
+        	int V = Integer.parseInt(st.nextToken());
+        	arr[i][0] = W;
+        	arr[i][1] = V;
+        }
+        
+        int[] dp = new int[K+1];
+        for (int i = 0; i < N; i++) {
+        	for (int j = K; j >= arr[i][0]; j--) {
+        		dp[j] = Math.max(dp[j], dp[j-arr[i][0]] + arr[i][1]);
+        	}
+        }
+        int ans = Arrays.stream(dp).max().getAsInt();
+        System.out.println(ans);
+    } // main
     
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] dp = new int[K+1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int W = Integer.parseInt(st.nextToken());
-			int V = Integer.parseInt(st.nextToken());
-			for (int j = K; j >= W; j--) {
-				dp[j] = Math.max(dp[j], dp[j-W] + V);
-			}
-		}
-		System.out.println(dp[K]);
-	} // main
 }
