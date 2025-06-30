0f4556d6890039b351c0a431cbe9b80864dc2c61
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-06-30 15:49:27 +0900

    [level 2] Title: 전화번호 목록, Time: 392.57 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,19 @@
 import java.util.*;
+// O(n)으로 줄이기
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        
-        for (int i = 0; i < phone_book.length; i++) {
-            set.add(phone_book[i]);
-        }
-        
-        for (String s : phone_book) {
-            for (int i = 0; i < s.length(); i++) {
-                if (set.contains(s.substring(0, i))) {
-                    return false;
-                }
+        // 정렬 -> 아스키 순서 -> 길이 순서
+        // 1부터 n-1까지 순회하면서 이전과 비교 후 startsWith()
+        Arrays.sort(phone_book, (a, b) -> {
+            if (a.equals(b)) {
+                return a.length() - b.length();
             }
+            return a.compareTo(b);
+        });
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i-1])) return false;
         }
-        
         boolean answer = true;
         return answer;
     }
