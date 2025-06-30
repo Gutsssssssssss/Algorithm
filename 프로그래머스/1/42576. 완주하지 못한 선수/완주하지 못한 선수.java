04383f7a9a9fcf0d4e36aa27860c40793fa32887
전정민 <77835002+Gutsssssssssss@users.noreply.github.com>
2025-06-30 14:30:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.71 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Set<String> set = new HashSet<>();
         Map<String, Integer> map = new HashMap<>();
-        
         for (String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            int num = map.getOrDefault(s, 0);
+            map.put(s, ++num);
         }
         
         for (String s : completion) {
-            map.put(s, map.get(s) - 1);
+            int num = map.get(s);
+            map.put(s, --num);
         }
         
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                return key;
+        Set<String> keySet = map.keySet();
+        for (String s : keySet) {
+            if (map.get(s) != 0) {
+                return s;
             }
         }
         
